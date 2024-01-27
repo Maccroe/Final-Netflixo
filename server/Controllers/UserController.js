1f2e66b9b48685b794a69c40eb0dc9f1cb4b37d4
Maccroe <amspike050@gmail.com>
2024-01-27 16:41:35 +0530

    upto update user profile

--- server/Controllers/UserController.js
+++ server/Controllers/UserController.js
@@ -47,4 +47,69 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 });
 
-export { registerUser };
+// @desc Login user
+// @route POST /api/users/login
+// @access Public
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    // find user in DB
+    const user = await User.findOne({ email });
+    // if user exists compare password with hashed password then send user data and token to client
+    if (user && (await bcrypt.compare(password, user.password))) {
+      res.json({
+        _id: user._id,
+        fullName: user.fullName,
+        email: user.email,
+        image: user.image,
+        isAdmin: user.isAdmin,
+        token: generateToken(user._id),
+      });
+      // if user not found or password not match send error message
+    } else {
+      res.status(401);
+      throw new Error("Invalid email or password");
+    }
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+// ********* PRIVATE CONTROLLER *********
+
+// @desc Update user profile
+// @route PUT /api/users/profile
+// @access Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const { fullName, email, image } = req.body;
+  try {
+    // find user in DB
+    const user = await User.findById(req.user._id);
+    // if user exists update user data and save it in DB
+    if (user) {
+      user.fullName = fullName || user.fullName;
+      user.email = email || user.email;
+      user.image = image || user.image;
+
+      const updatedUser = await user.save();
+      // send updated user data and token to client
+      res.json({
+        _id: updatedUser._id,
+        fullName: updatedUser.fullName,
+        email: updatedUser.email,
+        image: updatedUser.image,
+        isAdmin: updatedUser.isAdmin,
+        token: generateToken(updatedUser._id),
+      });
+    }
+    // else send error message
+    else {
+      res.status(404);
+      throw new Error("User not found");
+    }
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+export { registerUser, loginUser, updateUserProfile };
--- server/middlewares/Auth.js
+++ server/middlewares/Auth.js
@@ -1,4 +1,7 @@
 import jwt from "jsonwebtoken";
+import User from "../Models/UserModels.js";
+import asyncHandler from "express-async-handler";
+
 // @desc Authenticated used & get token
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -6,4 +9,33 @@ const generateToken = (id) => {
   });
 };
 
-export { generateToken };
+// protection middleware
+const protect = asyncHandler(async (req, res, next) => {
+  let token;
+  // check if token exists in headers
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    // set token from Bearer token in header
+    try {
+      token = req.headers.authorization.split(" ")[1];
+      // verify token and get user id
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      // get user id from decoded token
+      req.user = await User.findById(decoded.id).select("-password");
+      next();
+    } catch (error) {
+      console.error(error);
+      res.status(401);
+      throw new Error("Not authorized, token failed");
+    }
+  }
+  // if token doesn't exist in headers send error
+  if (!token) {
+    res.status(401);
+    throw new Error("Not authorized, no token");
+  }
+});
+
+export { generateToken, protect };
