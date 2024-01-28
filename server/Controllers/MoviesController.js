8dabdfb7a31dc9e79b3a6c082c4b201c852e6476
Maccroe <amspike050@gmail.com>
2024-01-28 22:18:04 +0530

    upto create movie review

--- server/Controllers/MoviesController.js
+++ server/Controllers/MoviesController.js
@@ -58,4 +58,118 @@ const getMovies = asyncHandler(async (req, res) => {
   }
 });
 
-export { importMovies, getMovies };
+// @desc    get movie by id
+// @route   GET /api/movies/:id
+// @access  Public
+
+const getMovieById = asyncHandler(async (req, res) => {
+  try {
+    // find movie by id in database
+    const movie = await Movie.findById(req.params.id);
+    // if the movie is found send it to the client
+    if (movie) {
+      res.json(movie);
+    }
+    // if the movie is not found send 404 error
+    else {
+      res.status(404);
+      throw new Error("Movie not found");
+    }
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+// @desc    Get top rated movies
+// @route   GET /api/movies/rated/top
+// @access  Public
+
+const getTopRatedMovies = asyncHandler(async (req, res) => {
+  try {
+    // find top rated movies
+    const movies = await Movie.find({}).sort({ rate: -1 });
+    // send top rated movies to the client
+    res.json(movies);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+// @desc    Get random movies
+// @route   GET /api/movies/random/all
+// @access  Public
+
+const getRandomMovies = asyncHandler(async (req, res) => {
+  try {
+    // find random movies
+    const movies = await Movie.aggregate([{ $sample: { size: 8 } }]);
+    // send random movies to the client
+    res.json(movies);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+// ************ PRIVATE CONTROLLERS ************
+
+// @desc    Create movie review
+// @route   POST /api/movies/:id/reviews
+// @access  Private
+
+const createMovieReview = asyncHandler(async (req, res) => {
+  const { rating, comment } = req.body;
+  try {
+    // find movie by id in database
+    const movie = await Movie.findById(req.params.id);
+
+    if (movie) {
+      // check if the user already reviewed this movie
+      const alreadyReviewed = movie.reviews.find(
+        (r) => r.userId.toString() === req.user._id.toString()
+      );
+      // if the user already reviewed this movie send 404 error
+      if (alreadyReviewed) {
+        res.status(400);
+        throw new Error("You already reviewed this movie");
+      }
+      // else create a new review
+      const review = {
+        userName: req.user.fullName,
+        userId: req.user._id,
+        userImage: req.user.image,
+        rating: Number(rating),
+        comment,
+      };
+      // push the new review to the reviews array
+      movie.reviews.push(review);
+      // increment the number of reviews
+      movie.numberOfReviews = movie.reviews.length;
+
+      // calculate the new rate
+      movie.rate =
+        movie.reviews.reduce((acc, item) => item.rating + acc, 0) /
+        movie.reviews.length;
+
+      // save the movie in database
+      await movie.save();
+      // send the new review to the client
+      res.status(201).json({
+        message: "Review added",
+      });
+    } else {
+      res.status(404);
+      throw new Error("Movie not found");
+    }
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+export {
+  importMovies,
+  getMovies,
+  getMovieById,
+  getTopRatedMovies,
+  getRandomMovies,
+  createMovieReview,
+};
