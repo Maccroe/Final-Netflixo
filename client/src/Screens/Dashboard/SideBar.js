df730f96cc67dde11405e6ef4afc46889d70c20d
Maccroe <amspike050@gmail.com>
2024-01-31 19:00:50 +0530

    logout successfully working

--- client/src/Screens/Dashboard/SideBar.js
+++ client/src/Screens/Dashboard/SideBar.js
@@ -1,55 +1,95 @@
 import React from "react";
 import { BsFillGridFill } from "react-icons/bs";
 import { FaHeart, FaListAlt, FaUsers } from "react-icons/fa";
-import { RiLockPasswordLine, RiMovie2Fill } from "react-icons/ri";
+import {
+  RiLockPasswordLine,
+  RiLogoutCircleLine,
+  RiMovie2Fill,
+} from "react-icons/ri";
 import { HiViewGridAdd } from "react-icons/hi";
 import { FiSettings } from "react-icons/fi";
 import Layout from "../../Layout/Layout";
 import { NavLink } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { logoutAction } from "../../Redux/Actions/userActions";
+import toast from "react-hot-toast";
 
 function SideBar({ children }) {
-  const SideLinks = [
-    {
-      name: "Dashboard",
-      link: "/dashboard",
-      icon: BsFillGridFill,
-    },
-    {
-      name: "Movies List",
-      link: "/movieslist",
-      icon: FaListAlt,
-    },
-    {
-      name: "Add Movie",
-      link: "/addmovie",
-      icon: RiMovie2Fill,
-    },
-    {
-      name: "Categories",
-      link: "/categories",
-      icon: HiViewGridAdd,
-    },
-    {
-      name: "Users",
-      link: "/users",
-      icon: FaUsers,
-    },
-    {
-      name: "Update Profile",
-      link: "/profile",
-      icon: FiSettings,
-    },
-    {
-      name: "Favorites Movies",
-      link: "/favorites",
-      icon: FaHeart,
-    },
-    {
-      name: "Change Password",
-      link: "/password",
-      icon: RiLockPasswordLine,
-    },
-  ];
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { userInfo } = useSelector((state) => state.userLogin);
+
+  // logout
+  const logoutHandler = () => {
+    dispatch(logoutAction());
+    toast.success("Logged out successfully");
+    navigate("/login");
+  };
+
+  const SideLinks = userInfo?.isAdmin
+    ? [
+        {
+          name: "Dashboard",
+          link: "/dashboard",
+          icon: BsFillGridFill,
+        },
+        {
+          name: "Movies List",
+          link: "/movieslist",
+          icon: FaListAlt,
+        },
+        {
+          name: "Add Movie",
+          link: "/addmovie",
+          icon: RiMovie2Fill,
+        },
+        {
+          name: "Categories",
+          link: "/categories",
+          icon: HiViewGridAdd,
+        },
+        {
+          name: "Users",
+          link: "/users",
+          icon: FaUsers,
+        },
+        {
+          name: "Update Profile",
+          link: "/profile",
+          icon: FiSettings,
+        },
+        {
+          name: "Favorites Movies",
+          link: "/favorites",
+          icon: FaHeart,
+        },
+        {
+          name: "Change Password",
+          link: "/password",
+          icon: RiLockPasswordLine,
+        },
+      ]
+    : userInfo
+    ? [
+        {
+          name: "Update Profile",
+          link: "/profile",
+          icon: FiSettings,
+        },
+        {
+          name: "Favorites Movies",
+          link: "/favorites",
+          icon: FaHeart,
+        },
+        {
+          name: "Change Password",
+          link: "/password",
+          icon: RiLockPasswordLine,
+        },
+      ]
+    : [];
+
   const active = "bg-dryGray text-subMain";
   const hover = "hover:text-white hover:bg-main";
   const inActive =
@@ -70,6 +110,12 @@ function SideBar({ children }) {
                 </NavLink>
               ))
             }
+            <button
+              onClick={logoutHandler}
+              className={`${inActive} ${hover} w-full `}
+            >
+              <RiLogoutCircleLine/> Log Out
+            </button>
           </div>
           <div
             data-aos="fade-up"
