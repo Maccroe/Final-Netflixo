b49ce4095ba27278c70cee364769f804f62b6d37
Maccroe <amspike050@gmail.com>
2024-02-03 23:11:26 +0530

    get movies, random movies, and top rated movies

--- client/src/Redux/APIs/MoviesServices.js
+++ client/src/Redux/APIs/MoviesServices.js
@@ -10,10 +10,28 @@ export const getAllMoviesService = async (
   rate,
   year,
   search,
-  pageNumber,
+  pageNumber
 ) => {
   const { data } = await Axios.get(
     `/movies?category=${category}&time=${time}&language=${language}&rate=${rate}&year=${year}&search=${search}&pageNumber=${pageNumber}`
   );
+  return data;
+};
+
+// get random movies Function
+export const getRandomMoviesService = async () => {
+  const { data } = await Axios.get("/movies/random/all");
+  return data;
+};
+
+// get movie by id Function
+export const getMovieByIdService = async (id) => {
+  const { data } = await Axios.get(`/movies/${id}`);
+  return data;
+};
+
+// get top rated movies Function
+export const getTopRatedMoviesService = async () => {
+  const { data } = await Axios.get("/movies/rated/top");
   return data;
 };
