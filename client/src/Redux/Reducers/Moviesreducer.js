ce10c3b31fabf64ebe8f887a5a5b9d94acef432e
Maccroe <amspike050@gmail.com>
2024-02-04 15:38:37 +0530

    successfully done upto create review

--- client/src/Redux/Reducers/Moviesreducer.js
+++ client/src/Redux/Reducers/Moviesreducer.js
@@ -50,7 +50,6 @@ export const movieDetailsReducer = (state = { movie: {} }, action) => {
   }
 };
 
-
 // GET TOP RATED MOVIES
 export const movieTopRatedReducer = (state = { movies: [] }, action) => {
   switch (action.type) {
@@ -60,6 +59,22 @@ export const movieTopRatedReducer = (state = { movies: [] }, action) => {
       return { isLoading: false, movies: action.payload };
     case moviesConstants.MOVIE_TOP_RATED_FAIL:
       return { isLoading: false, isError: action.payload };
+    default:
+      return state;
+  }
+};
+
+// CREATE REVIEW
+export const createReviewReducer = (state = {}, action) => {
+  switch (action.type) {
+    case moviesConstants.CREATE_REVIEW_REQUEST:
+      return { isLoading: true };
+    case moviesConstants.CREATE_REVIEW_SUCCESS:
+      return { isLoading: false, isSuccess: true };
+    case moviesConstants.CREATE_REVIEW_FAIL:
+      return { isLoading: false, isError: action.payload };
+    case moviesConstants.CREATE_REVIEW_RESET:
+      return {};
     default:
       return state;
   }
