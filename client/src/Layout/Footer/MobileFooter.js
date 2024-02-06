dc5724f8947d63021deae30cb2895bf3e49c7799
Maccroe <amspike050@gmail.com>
2024-02-06 22:34:02 +0530

    fully done

--- client/src/Layout/Footer/MobileFooter.js
+++ client/src/Layout/Footer/MobileFooter.js
@@ -3,11 +3,14 @@ import { NavLink } from "react-router-dom";
 import { BsCollectionPlay } from "react-icons/bs";
 import { FiHeart, FiUserCheck } from "react-icons/fi";
 import { CgMenuBoxed } from "react-icons/cg";
-import MenuDrawer from './../../Components/Drawer/MenuDrawer';
-import { SidebarContext } from './../../Context/DrawerContext';
+import MenuDrawer from "./../../Components/Drawer/MenuDrawer";
+import { SidebarContext } from "./../../Context/DrawerContext";
+import { useSelector } from "react-redux";
 
 function MobileFooter() {
-  const { mobileDrawer, toggleDrawer } = useContext(SidebarContext)
+  const { mobileDrawer, toggleDrawer } = useContext(SidebarContext);
+  const { likedMovies } = useSelector((state) => state.userGetFavoriteMovies);
+  const { userInfo } = useSelector((state) => state.userLogin);
   const active = "bg-white text-main";
   const inActive =
     "transitions text-2xl flex-colo hover:bg-white hover:text-main text-bgdry rounded-md px-4 py-3";
@@ -17,7 +20,7 @@ function MobileFooter() {
   return (
     <>
       <div className="flex flex-col h-full justify-between align-middle bg-white rounded cursor-pointer overflow-y-scroll flex-grow w-full">
-      <MenuDrawer drawerOpen={mobileDrawer} toggleDrawer={toggleDrawer} />
+        <MenuDrawer drawerOpen={mobileDrawer} toggleDrawer={toggleDrawer} />
       </div>
       <footer className="lg:hidden fixed z-50 bottom-0 w-full px-1">
         <div className="bg-dry rounded-md flex-btn w-full p-1">
@@ -27,12 +30,21 @@ function MobileFooter() {
           <NavLink to="/favorites" className={Hover}>
             <div className="relative">
               <div className="w-5 h-5 flex-colo rounded-full text-xs bg-subMain text-white absolute -top-5 -right-1">
-                3
+                {likedMovies?.length > 0 ? likedMovies?.length : 0}
               </div>
               <FiHeart />
             </div>
           </NavLink>
-          <NavLink to="/login" className={Hover}>
+          <NavLink
+            to={
+              userInfo
+                ? userInfo.isAdmin
+                  ? "/dashboard"
+                  : "/profile"
+                : "/login"
+            }
+            className={Hover}
+          >
             <FiUserCheck />
           </NavLink>
           <button onClick={toggleDrawer} className={inActive}>
